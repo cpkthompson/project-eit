43549aa4a6bcb61f1c406f6e057a01da6483052f
Charles Thompson <thompsoncharles@live.com>
2018-04-06 23:49:47 +0000

    Switched out ternary expression computing age and used timeAgo library

--- js/eit.js
+++ js/eit.js
@@ -1,13 +1,14 @@
-angular.module('eit', [])
-  .controller('EITController', function EITController() {
-    this.givenNames = "";
-    this.lastName = "";
-    this.gender = "";
-    this.dob = "";
-    var currentdate = new Date();
-    var datetime = currentdate.getDate() + "-" + (currentdate.getMonth() + 1) + "-"
-      + currentdate.getFullYear() + "-" + currentdate.getHours() + "-"
-      + currentdate.getMinutes() + "-" + currentdate.getSeconds();
-    console.log(currentdate.getMilliseconds())
+angular.module('eit', [
+    'yaru22.angular-timeago',
+])
+    .controller('EITController', function EITController() {
+        this.givenNames = "";
+        this.lastName = "";
+        this.gender = "";
+        this.dob = "";
+        var currentdate = new Date();
+        var datetime = currentdate.getDate() + "-" + (currentdate.getMonth() + 1) + "-"
+            + currentdate.getFullYear() + "-" + currentdate.getHours() + "-"
+            + currentdate.getMinutes() + "-" + currentdate.getSeconds();
 
-  });
+    });
