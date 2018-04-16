f0435b8ade66e96f88095542c5094d0e49fd63e3
Charles Thompson <thompsoncharles@live.com>
2018-04-16 22:42:23 +0000

    Tried again and again and again to make this thing work. Still. AngularJS, I'm glad you died!

--- js/eit.js
+++ js/eit.js
@@ -1,13 +1,38 @@
 angular.module('eit', ['eitModule'])
     .controller('EITController', ['eitService', function EITController(eitService) {
 
-        this.name = "";
+        this.name = ""
+        
 
-        this.list = function list() {
+        if (this.editMode === true) {
+            
+        } else {
+            this.addEit = function addEit() {
+                return eitService.addEit(this.name);
+            };
+        };
+
+        this.editMode = function editMode(){
+            return eitService.editMode;
+        }
+        
+
+        
+    }])
+    .controller('DetailController', ['eitService', function DetailController(eitService) {
+        
+        this.getCurrentEit = function getCurrentEit() {
+            return eitService.getCurrentEit;
+        };
+        
+    }])
+    .controller('ListController', ['eitService', function ListController(eitService) {
+
+        this.getEitList = function list() {
             return eitService.list;
         };
 
-        this.addEit = function addEit() {
-            return eitService.addEit(this.name);
-        };
+        this.setCurrentEit = function setCurrentEit (name){
+            return eitService.setCurrentEit(name)
+        }
     }]);
--- js/eitModule.js
+++ js/eitModule.js
@@ -2,13 +2,23 @@ angular.module('eitModule', [])
     .factory('eitService', function () {
         let list = ["Charles Thompson", "Philisiah Mwaluma"];
 
+        let getCurrentEit;
+        let editMode = false;
 
         let addEit = function (eit) {
             return list.push(eit);
         };
 
+        let setCurrentEit = function (eit){
+            this.editMode = true;
+            return this.getCurrentEit = eit;
+        }
+
         return {
             list: list,
-            addEit: addEit
+            addEit: addEit,
+            setCurrentEit: setCurrentEit,
+            getCurrentEit: getCurrentEit,
+            editMode: editMode,
         };
     });
